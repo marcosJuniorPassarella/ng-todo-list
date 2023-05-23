1086202ce7ffb5a5a0ccc4dd7b6b442723c70f32
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-05-23 16:48:51 -0300

    Generate new standalone components

---
+++ src/app/components/todo-card/todo-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo-card.component.html',
+  styleUrls: []
+})
+export class TodoCardComponent {
+
+}
---
+++ src/app/components/todo-form/todo-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo-form.component.html',
+  styleUrls: []
+})
+export class TodoFormComponent {
+
+}
