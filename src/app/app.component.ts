9af7cce7a06adbeebc6899cfff1bd59ebcc69481
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-05-27 14:51:22 -0300

    Added updates in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
+import { TodoCardComponent } from './components/todo-card/todo-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, TodoCardComponent],
   templateUrl: './app.component.html',
   styleUrls: [],
 })
