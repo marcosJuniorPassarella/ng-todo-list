246ddaae1b22ceefc9ce3a142aa707b9ff042736
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-05-28 15:25:22 -0300

    Updated Todo model

--- src/app/models/todo.model.ts
+++ src/app/models/todo.model.ts
@@ -1,8 +1,8 @@
 export class Todo {
   constructor(
-    public id: Number,
-    public title: String,
-    public description: String,
-    public done: Boolean
+    public id: number,
+    public title: string,
+    public description: string,
+    public done: boolean
   ) {}
 }
