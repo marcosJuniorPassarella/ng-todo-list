c96980a4d6f3566058bd65faaa8a92b66e9cd209
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-05-28 15:25:39 -0300

    finished todo signal service

--- src/app/services/todo-signals.service.ts
+++ src/app/services/todo-signals.service.ts
@@ -5,14 +5,14 @@ import { Todo } from '../models/todo.model';
   providedIn: 'root',
 })
 export class TodoSignalsService {
-  private todosState = signal<Array<Todo> | null>([]);
+  public todosState = signal<Array<Todo>>([]);
 
   public updateTodos({ id, title, description, done }: Todo): void {
     if ((title && id && description !== null) || undefined) {
       this.todosState.mutate((todos) => {
         if (todos !== null) {
           todos.push(new Todo(id, title, description, done));
-          console.log('TODOS', this.todosState());
+          console.log('TODO ADICIONADO', this.todosState());
         }
       });
     }
