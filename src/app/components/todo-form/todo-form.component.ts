31c3c99f732d5f8706ed26deefca515e31bc5fb4
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-05-28 15:25:09 -0300

    Implement changes in the todo form component

--- src/app/components/todo-form/todo-form.component.ts
+++ src/app/components/todo-form/todo-form.component.ts
@@ -1,9 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import {
   FormControl,
   FormGroup,
@@ -11,8 +7,13 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatDialogRef } from '@angular/material/dialog';
 import { HeaderComponent } from '../header/header.component';
+import { TodoSignalsService } from 'src/app/services/todo-signals.service';
 
 @Component({
   selector: 'app-todo-form',
@@ -30,8 +31,11 @@ import { HeaderComponent } from '../header/header.component';
   styleUrls: [],
 })
 export class TodoFormComponent {
-  public todoForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  private todosSignalsService = inject(TodoSignalsService);
+  public allTodos = this.todosSignalsService.todosState();
+
+  public todosForm = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.minLength(3)]),
     description: new FormControl('', [
       Validators.required,
       Validators.minLength(5),
@@ -41,8 +45,15 @@ export class TodoFormComponent {
   constructor(public dialogRef: MatDialogRef<HeaderComponent>) {}
 
   public handleCreateNewTask(): void {
-    this.dialogRef.close();
-    console.log(this.todoForm);
+    if (this.todosForm.value && this.todosForm.valid) {
+      const title = String(this.todosForm.controls['title'].value);
+      const description = String(this.todosForm.controls['description'].value);
+      const id = this.allTodos.length > 0 ? this.allTodos.length + 1 : 1;
+      const done = false;
+
+      this.todosSignalsService.updateTodos({ id, title, description, done });
+      this.dialogRef.close();
+    }
   }
 
   public handleCloseModal(): void {
